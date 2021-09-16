df06ee4eecaf11a6055ad82e1d4546aa4f87c7bf
Erick Lima <70773204+ErickLima13@users.noreply.github.com>
2021-09-16 18:14:34 -0300

    iniciei projeto

---
+++ Assets/GameFolder/Player/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 vel;
+
+    public Transform floorCollider;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        // faz o player pular
+        if (Input.GetButtonDown("Jump") && floorCollider.GetComponent<FloorCollider>().canJump == true)
+        {
+            rb.velocity = Vector2.zero;
+            floorCollider.GetComponent<FloorCollider>().canJump = false;
+            rb.AddForce(new Vector2(0, 150));
+
+        }
+
+        // faz o player anda pra esquerda e direita
+        vel = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = vel;
+    }
+
+    
+}

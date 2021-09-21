c719775a2591a0e0132af519e4ead30d66cb579a
Erick Lima <70773204+ErickLima13@users.noreply.github.com>
2021-09-21 16:58:13 -0300

    enemies add/traps
    inclui dois inimigos e três tipos de armadilhas.

---
+++ Assets/GameFolder/Trap/BearTrap/Script/BearTrap.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTrap : MonoBehaviour
+{
+    Transform player;
+    public Transform skin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player = collision.transform;
+
+            skin.GetComponent<Animator>().Play("Stuck", -1);
+
+            player.position = transform.position;
+            player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+            GetComponent<BoxCollider2D>().enabled = false;
+
+            player.GetComponent<PlayerController>().enabled = false;
+            Invoke("ReleasePlayer", 2);
+        }
+    }
+
+    public void ReleasePlayer()
+    {
+        player.GetComponent<PlayerController>().enabled = true;
+        
+    }
+}
---
+++ Assets/GameFolder/Trap/SpikeTrap/Script/SpikeTrap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player = collision.transform;
+
+            player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 200));
+            player.GetComponent<Character>().life--;
+
+        }
+    }
+
+}
